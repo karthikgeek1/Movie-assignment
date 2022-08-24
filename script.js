1d86c67c3ded128b5104a6fb0a28960d343bb217
karthikgeek1 <kjdnskjn>
2022-08-24 11:43:16 +0530

    movies

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const create = document.getElementById("create");
-const done = document.getElementById("done")
+const done = document.getElementById("done");
 const close = document.getElementsByClassName("x-color")[0];
 const wrap = document.getElementById("wrap");
-
+const data=[];
 const container = document.getElementById("container")
 create.addEventListener("click", ()=>{
     wrap.style.display="block"
@@ -13,30 +13,71 @@ close.addEventListener("click", ()=>{
 })
 done.addEventListener("click", (e)=>{
     e.preventDefault();
-    setTimeout(()=>{trailer.value="", moviename.value="", imdb.value="", Actors.value="", category.value=""}, 1000)
+    let moviename = document.getElementById("moviename").value;
+    // console.log(moviename)
+    let actors = document.getElementById("Actors").value;
+    // console.log(actors);
+    let trailer = document.getElementById("trailer").value;
+    // console.log(trailer);
+    let imdb = document.getElementById("imdb").value;
+    // console.log(imdb);
+    let category = document.getElementById("category").value;
+    // console.log(category);
+
+    let obj={
+        Moviename:moviename,
+        Actors:actors,
+        Trailer:trailer,
+        IMDB:imdb,
+        Category:category
+    }
+    data.push(obj)
+    // console.log(data)
     wrap.style.display="none"
-    let newMovie = document.createElement("div")
-    newMovie.classList.add("movie")
-    newMovie.innerHTML=`<i class="fa-solid fa-x delete"></i>
-    <div class="hw">${trailer.value}</div>
-    <h4><span class="descrip">Movie Name: </span><span class="ans">${moviename.value}</span>
-    <h4><span class="descrip">Actors: </span><span class="ans">${Actors.value}</span>
-    <h4><span class="descrip">Category: </span><span class="ans">${category.value}</span>
-    <h4><span class="descrip">IMDB rating: </span><span class="ans">${imdb.value}</span>`
-    container.append(newMovie)
-    const del = document.getElementsByClassName("delete")
-    const DEL=Array.from(del)
-    DEL.map((Element)=>{
-        Element.addEventListener("click",()=>{
-            Element.parentElement.remove()
-        })
-    })
-    const iframe = document.getElementsByTagName("iframe")
-    const IFRAME = Array.from(iframe)
-    IFRAME.map((e)=>{
-        e.height="250"
-        e.width="300"
-        console.log(e);
-    })
+    show(data)
 })
 
+function show(newdata){
+container.innerHTML=null
+    newdata.map((d)=>{
+        const newMovie=document.createElement("div");
+        newMovie.classList.add("movie")
+        newMovie.innerHTML=`<i class="fa-solid fa-x delete"></i>
+                            <div class="hw">${d.Trailer}</div>
+                            <h4><span class="descrip">Movie:</span><span class="ans">${d.Moviename}</span></h4>
+                            <h4><span class="descrip">Actors:</span><span class="ans">${d.Actors}</span></h4>
+                            <h4><span class="descrip">rating:</span><span class="ans">${d.IMDB}</span></h4>
+                            <h4><span class="descrip">category:</span><span class="ans">${d.Category}</span></h4>`
+                            container.appendChild(newMovie)
+        
+    })
+}
+const del = document.getElementsByClassName("delete")
+const del1=Array.from(del)
+del1.map((element)=>{
+    element.addEventListener("click",(e)=>{
+        element.parentElement.remove()
+    })
+})
+const sort = document.getElementById("sort")
+sort.addEventListener("click", ()=>{
+   let sortedata = data.sort(function(a,b){return a.IMDB-b.IMDB})
+   console.log(sortedata);
+   show(sortedata)
+})
+const action = document.getElementById("action")
+action.addEventListener("click", ()=>{
+    let filterdata = data.filter((e)=>{return e.Category==="action"})
+    // console.log(filterdata);
+    show(filterdata)
+})
+const comedy=document.getElementById("comedy")
+comedy.addEventListener("click", ()=>{
+    let fdata = data.filter((e)=>{return e.Category==="comedy"})
+    show(fdata)
+})
+const thriller = document.getElementById("thriller")
+thriller.addEventListener("click", ()=>{
+    let fdata1 = data.filter((e)=>{return e.Category==="thriller"})
+    show(fdata1)
+})
